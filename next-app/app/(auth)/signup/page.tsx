6c49479f5cc2d879adf8a7c73726c6d568271c77
priyangshu som <priyangshusom975@gmail.com>
2026-03-12 15:04:29 +0530

    new initial setup

---
+++ next-app/app/(auth)/signup/page.tsx
@@ -0,0 +1,13 @@
+
+
+
+export default function SignupPage(){
+
+   return(
+      <>
+         <div className="signup-card flex flex-col items-center justify-center gap-4 rounded-lg border p-8">
+            <h1 className="text-2xl font-bold">Create Account</h1>
+         </div>
+      </>
+   )
+}
